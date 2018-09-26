6007430ed6a0f5b9ff47c2a622cd5889cdf8a3a9
Greg Foreman <gfore@mindspring.com>
2018-09-26 11:36:24 -0400

    added gothouses.py, a dict of got houses to url

--- got.py
+++ got.py
@@ -11,7 +11,7 @@ hotpieActor = ''
 notOnTV = 0
 targaryens = 0
 houses = {}
-
+house_dict = {}
 
 for i in characters:
     if i['name'].startswith('A'): startsA += 1
@@ -23,13 +23,20 @@ for i in characters:
     if i['culture'] == 'Valyrian': numVal += 1
     if i['name'] == 'Hot Pie': hotpieActor = i['playedBy'][0]
     if i['tvSeries'] == [""]: notOnTV += 1
-    if 'Targaryen' in i['name']: targaryens += 1
+    if 'Targaryen' in i['name']:
+        targaryens += 1
+        print(i['name'])
 
     allegs = i['allegiances']
-    for house in allegs:
-        req = Request(house, headers={'User-Agent': 'Mozilla/5.0'})
-        webpage = urlopen(req).read().decode('utf-8')
-        housename = webpage[webpage.find("name")+7: webpage.find("region")-3]
+    
+    for house in allegs:                                       # house is a URL
+        housename = house_dict.get(house, '')                  # if housename already found, then use it, don't get url again
+        if housename == '':
+            print('new')
+            req = Request(house, headers={'User-Agent': 'Mozilla/5.0'})
+            webpage = urlopen(req).read().decode('utf-8')
+            housename = webpage[webpage.find("name")+7: webpage.find("region")-3]
+            house_dict[house] = housename
         print(housename)
         houses[housename] = houses.get(housename, 0) + 1
 
@@ -45,6 +52,7 @@ print('How many characters have last name "Targaryen"? Ans:', targaryens)
 print('Histogram of the Houses:')
 print(houses)
 print()
+print(house_dict)
 
 ## dict_keys(['url', 'name', 'gender', 'culture', 'born', 'died', 'titles', 'aliases', 'father', 'mother', 'spouse', 'allegiances', 'books', 'povBooks', 'tvSeries', 'playedBy'])
 
--- lettersummary.py
+++ lettersummary.py
@@ -2,17 +2,17 @@
 # a dictionary containing the tally of how many times each letter in the
 # alphabet was used in the word. 
 
-word = input("\nPlease enter a word: ")     # append .lower() here for case insensitive
+word = input("\nPlease enter a word: ").lower()     # append .lower() here for case insensitive
 
 histogram_dict = {}
 
 for letter in word:
-    if letter in histogram_dict:
-        histogram_dict[letter] += 1
-    else:
-        histogram_dict[letter] = 1
+    # if letter in histogram_dict:
+    #     histogram_dict[letter] += 1
+    # else:
+    #     histogram_dict[letter] = 1
 
-    #histogram_dict[letter] = histogram_dict.get(letter, 0) + 1
+    histogram_dict[letter] = histogram_dict.get(letter, 0) + 1
 
 print(histogram_dict)
 print()
